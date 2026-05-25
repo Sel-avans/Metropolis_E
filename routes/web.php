bac919a0ab7334116a458f2071498ed39f96dc68
KionKessels <kion.kessels@gmail.com>
2026-05-26 01:25:25 +0200

    Update web.php
    de web.php geüpdate op basis van de pagePolicies!

--- routes/web.php
+++ routes/web.php
@@ -8,42 +8,72 @@ use App\Http\Controllers\FunctionController;
 use App\Http\Controllers\EffectsController;
 use App\Http\Controllers\FunctionManagementController;
 use App\Http\Controllers\ConditionsController;
+use App\Policies\PagePolicy;
 
-Route::get('/functions', [FunctionManagementController::class, 'index'])->name('functions.index');
-Route::get('/functions/create', [FunctionManagementController::class, 'create'])->name('functions.create');
-Route::post('/functions', [FunctionManagementController::class, 'store'])->name('functions.store');
-Route::get('/functions/{function}/edit', [FunctionManagementController::class, 'edit'])->name('functions.edit');
-Route::put('/functions/{function}', [FunctionManagementController::class, 'update'])->name('functions.update');
-Route::delete('/functions/{function}', [FunctionManagementController::class, 'destroy'])->name('functions.destroy');
-
+// Publieke route
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/grid', [GridController::class, 'index'])->name('grid');
-Route::get('/library', [FunctionController::class, 'index'])->name('library');
-
-Route::get('/effects', [EffectsController::class, 'index'])->name('effects.index');
-Route::post('/effects/update', [EffectsController::class, 'update'])->name('effects.update');
-
-Route::get('/functions/manage', function () { return view('functions.manage'); })->name('functions.manage');
-
-Route::post('/grid/update', [GridController::class, 'update']);
-Route::get('/qol/details', [QoLController::class, 'details'])->name('qol.details');
-Route::get('/qol/cell/{row}/{col}', [QoLController::class, 'cellHoverDetails']); // Nieuwe route voor de cel hover details
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+// Alle routes hierbinnen vereisen dat de gebruiker is ingelogd
 Route::middleware('auth')->group(function () {
+
+    // Dashboard & Profiel routes
+    Route::get('/dashboard', function () { return view('dashboard'); })->middleware('verified')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // === GRID & KWALITEIT VAN DE LEFOMGEVING (QoL) ===
+    Route::middleware('can:CanViewGridPage,' . PagePolicy::class)->group(function () {
+        Route::get('/grid', [GridController::class, 'index'])->name('grid');
+        Route::get('/qol/details', [QoLController::class, 'details'])->name('qol.details');
+        Route::get('/qol/cell/{row}/{col}', [QoLController::class, 'cellHoverDetails']);
+        
+        // Functies plaatsen/verwijderen op het grid (City Planner)
+        Route::post('/grid/update', [GridController::class, 'update'])->middleware('can:CanPlaceFunctions,' . PagePolicy::class);
+        Route::delete('/grid/cell/{cell}/function', [GridController::class, 'removeFunction'])->middleware('can:CanPlaceFunctions,' . PagePolicy::class);
+    });
+
+    // === FUNCTIONS / FUNCTIEBEHEER ===
+    // Pagina bekijken (City planner & Administrator)
+    Route::middleware('can:CanViewFunctionPage,' . PagePolicy::class)->group(function () {
+        Route::get('/functions', [FunctionManagementController::class, 'index'])->name('functions.index');
+        Route::get('/library', [FunctionController::class, 'index'])->name('library');
+        Route::get('/functions/manage', function () { return view('functions.manage'); })->name('functions.manage');
+    });
+
+    // Functies aanmaken, aanpassen en verwijderen (Alleen City Planner)
+    Route::middleware('can:CanPlaceFunctions,' . PagePolicy::class)->group(function () {
+        Route::get('/functions/create', [FunctionManagementController::class, 'create'])->name('functions.create');
+        Route::post('/functions', [FunctionManagementController::class, 'store'])->name('functions.store');
+        Route::get('/functions/{function}/edit', [FunctionManagementController::class, 'edit'])->name('functions.edit');
+        Route::put('/functions/{function}', [FunctionManagementController::class, 'update'])->name('functions.update');
+        Route::delete('/functions/{function}', [FunctionManagementController::class, 'destroy'])->name('functions.destroy');
+    });
+
+    // === EFFECTS ===
+    Route::middleware('can:CanViewEffectsPage,' . PagePolicy::class)->group(function () {
+        Route::get('/effects', [EffectsController::class, 'index'])->name('effects.index');
+        // Updaten van effecten mag alleen de Administrator (CanChangeQOLEffect)
+        Route::post('/effects/update', [EffectsController::class, 'update'])->middleware('can:CanChangeQOLEffect,' . PagePolicy::class);
+    });
+
+    // === CONDITIONS (Resource Route) ===
+    // Bekijken mag via CanViewConditionsPage
+    Route::get('/conditions', [ConditionsController::class, 'index'])
+        ->middleware('can:CanViewConditionsPage,' . PagePolicy::class)
+        ->name('conditions.index');
+
+    // Aanmaken/wijzigen/verwijderen mag alleen de Administrator
+    Route::middleware('can:CanEditConditions,' . PagePolicy::class)->group(function () {
+        Route::get('/conditions/create', [ConditionsController::class, 'create'])->name('conditions.create');
+        Route::post('/conditions', [ConditionsController::class, 'store'])->name('conditions.store');
+        Route::get('/conditions/{condition}/edit', [ConditionsController::class, 'edit'])->name('conditions.edit');
+        Route::put('/conditions/{condition}', [ConditionsController::class, 'update'])->name('conditions.update');
+        Route::delete('/conditions/{condition}', [ConditionsController::class, 'destroy'])->name('conditions.destroy');
+    });
+
 });
-
-Route::delete('/grid/cell/{cell}/function', [GridController::class, 'removeFunction']);
-
-Route::resource('conditions', ConditionsController::class)->except(['show']);
 
 require __DIR__.'/auth.php';
